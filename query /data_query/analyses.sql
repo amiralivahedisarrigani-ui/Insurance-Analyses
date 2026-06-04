f81161042975ebdc0cd24725339f03eff2319ec5
amiralivahedisarrigani-ui <amiralivahedisarrigani@gmail.com>
2026-06-04 15:38:31 +0400

    Update analyses.sql

--- query /data_query/analyses.sql
+++ query /data_query/analyses.sql
@@ -1,14 +1,17 @@
+Use Insurance;
 /*
  * PROJECT: Insurance Analysis
  * AUTHOR: Amir Ali Vahedi
  * DESCRIPTION: This report contains the analysis for 10 key business questions.
  */
 
--- 1. Which age group files the most claims?
+-- 1. Age demographic of client and group with highest claims?
 
-SELECT 
-	Age_Group,
-    COUNT(Customer_ID) AS count_customer,
-    SUM(claim_count) AS SUM_Claim
-FROM Insurance.Insurance_data
-GROUP BY Age_Group;
+	SELECT 
+        Age_Group,
+        Round(AVG(charges),2) AS Average_Charges,
+        SUM(claim_count) AS Total_Claim
+    FROM Insurance.Insurance_data
+    GROUP BY Age_Group
+    ORDER BY Total_Claim DESC;
+
